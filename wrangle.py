3437e3a7147d80e150515c56e62b27cc1b02a538
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-05-20 20:33:53 -0500

    updates to split function

--- wrangle.py
+++ wrangle.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 
