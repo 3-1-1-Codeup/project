9a88d3822e3ba08bc15a0bf3488ada15782035dd
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-05-20 20:16:52 -0500

    fixing imports

--- workbooks/gabby/wrangle.py
+++ workbooks/gabby/wrangle.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
+
+#-----------------------------------------------------------------------------
 
 def get_311_data():
     '''
@@ -14,7 +15,6 @@ def get_311_data():
     '''
     df= pd.read_csv('service_calls.csv')
     return df
-
 #-----------------------------------------------------------------------------
 
 # Set the index
@@ -87,11 +87,19 @@ def create_delay_columns(df):
                                 labels = ['Extremely Late Response', 'Very Late Response', 
                                           'Late Response', "On Time Response", "Early Response", 
                                           'Very Early Response', 'Extremely Early Response'])
-    # add a new category for level of delay
-    df["level_of_delay"] = df['level_of_delay'].cat.add_categories('Still Open')
-    # replace nulls in level of delay with the new "Still Open" category
-    df["level_of_delay"].fillna("Still Open", inplace=True)
+    # drop nulls in these columns
+    df.dropna(subset=['days_open'], how='all', inplace=True)
+    df.dropna(subset=['level_of_delay'], how='all', inplace=True)
     # return new df
+    return df
+
+#-----------------------------------------------------------------------------
+
+def handle_outliers(df):
+    '''removes outiers from df'''
+    # remove outliers from days_open
+    df = df[df.days_open < 1400]
+    # return df
     return df
 
 #-----------------------------------------------------------------------------
@@ -151,7 +159,6 @@ def clean_column_names(df):
                     'CaseStatus': 'case_status', 'SourceID':'source_id', 'XCOORD': 'longitude', 'YCOORD': 'latitude',
                     'Report Starting Date': 'report_start_date', 'Report Ending Date': 'report_end_date'
                       })
-    df['zipcode'] = df['address'].str.extract(r'(\d{5}\-?\d{0,4})')
     return df
 
 #-----------------------------------------------------------------------------
@@ -165,37 +172,17 @@ def clean_311(df):
     df = handle_nulls(df)
     # creating delay involved columns
     df = create_delay_columns(df)
+    # handle outliers
+    df = handle_outliers(df)
     # merge reasons
     df = clean_reason(df)
     # rename columns
     df = clean_column_names(df)
     # return df
     return df
-#~~~~~~~~~~~~~~~~~~
 
-def clean_reason(df):
-    '''
-    This function will take in the service call df and replace the content of REASONNAME column with condensed names
-    '''
-    df['REASONNAME'] = df['REASONNAME'].replace(['Waste Collection', 'Solid Waste', 'Brush'], 'waste')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Code Enforcement', 'Code Enforcement (Internal)', 'Code Enforcement (IntExp)'], 'code')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Field Operations', 'Vector'], 'field')
-    df['REASONNAME'] = df['REASONNAME'].replace('Miscellaneous', 'misc')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Traffic Operations', 'Traffic Engineering Design', 'Traffic Issue Investigation'], 'traffic')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Streets', 'Signals', 'Signs and Markings'], 'streets')
-    df['REASONNAME'] = df['REASONNAME'].replace('Trades', 'trades')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Stormwater', 'Storm Water', 'Storm Water Engineering'], 'storm')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Small Business', 'Food Establishments', 'Shops (Internal)', 'Shops'], 'business')
-    df['REASONNAME'] = df['REASONNAME'].replace('Workforce Development', 'workforce')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Customer Service', '311 Call Center', 'Director\'s Office Horizontal'], 'customer_service')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Land Development', 'Clean and Green', 'Urban Forestry', 'Natural Resources', 'Park Projects', 'Tree Crew', 'District 2', 'Clean and Green Natural Areas'], 'land')
-    df['REASONNAME'] = df['REASONNAME'].replace('Facility License', 'license')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Dangerous Premise','Historic Preservation', 'Engineering Division'], 'buildings')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Graffiti (IntExp)', 'General Sanitation', 'Graffiti'], 'cleanup')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Waste Collection', 'Solid Waste'], 'waste')
-    df['REASONNAME'] = df['REASONNAME'].replace(['Waste Collection', 'Solid Waste'], 'waste')
-    return df
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#-----------------------------------------------------------------------------
+
 
 def split_separate_scale(df, stratify_by= None):
     '''
@@ -206,104 +193,38 @@ def split_separate_scale(df, stratify_by= None):
     finally it will return all dataframes individually
     '''
     # split data into train, validate, test
-    train, validate, test = split(df, random_state= 123, stratify_by= None)
-     # seperate target variable
-    X_train, y_train, X_validate, y_validate, X_test, y_test = separate_y(train, validate, test)
-    # scale numeric variable
-    train_scaled, validate_scaled, test_scaled = scale_data(X_train, X_validate, X_test)
-    return train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test, train_scaled, validate_scaled, test_scaled
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def mother_function():
-    df= pd.read_csv('service_calls.csv')
-    # Drop category, report starting date, and report ending date
-    df.drop(columns=['Category', 
-                 'Report Starting Date', 
-                 'Report Ending Date' ], inplace=True)
-    # Set index to case id
-    df.set_index('CASEID')
-    # drop null values
-    df.dropna(subset = ['SLA_Date', 'XCOORD', 'YCOORD'], inplace = True)
-    # replace null values
-    df.fillna({'Dept': 'Unknown'}, inplace = True)
-    # remove city council department
-    df = df[df.Dept != 'City Council']
-   # make sure the open and closed date columns are formatted in datetime format
-    df['OPENEDDATETIME'] = pd.to_datetime(df['OPENEDDATETIME'])
-    df['CLOSEDDATETIME'] = pd.to_datetime(df['CLOSEDDATETIME'])
-    df['SLA_Date'] = pd.to_datetime(df['SLA_Date'])
-    # create new number of days open feature
-    df['days_open'] = df['CLOSEDDATETIME'] - df['OPENEDDATETIME']
-    # Create new column to hold days before needed resolution
-    df['resolution_days_due'] = df['SLA_Date'] - df['OPENEDDATETIME']
-    # Convert to string format insted of timedelta
-    df['days_open'] = df.days_open // pd.Timedelta('1d')
-    # Convert to string format insted of timedelta
-    df['resolution_days_due'] = df.resolution_days_due // pd.Timedelta('1d')
-    # create new feature to show how long it took to resolve compared to resolution due date
-    df['days_before_or_after_due'] = df.resolution_days_due - df.days_open
-        # postitive means before days before due data and negative means number of days after due
-    # bin how long it took compare to due date to get level of delay
-    df['level_of_delay'] = pd.cut(df.days_before_or_after_due, 
-                                bins = [-700,-500,-300,-100,0,100,300,500],
-                                labels = ['Extremely Late Response', 'Very Late Response', 
-                                          'Late Response', "On Time Response", "Early Response", 
-                                          'Very Early Response', 'Extremely Early Response'])
-    # add a new category for level of delay
-    df["level_of_delay"] = df['level_of_delay'].cat.add_categories('Still Open')
-    # replace nulls in level of delay with the new "Still Open" category
-    df["level_of_delay"].fillna("Still Open", inplace=True)
-   # replace with waste
-    df['REASONNAME'] = df['REASONNAME'].replace(['Waste Collection', 'Solid Waste', 'Brush'], 'waste')
-    # replace with code
-    df['REASONNAME'] = df['REASONNAME'].replace(['Code Enforcement', 'Code Enforcement (Internal)', 'Code Enforcement (IntExp)'], 'code')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Field Operations', 'Vector'], 'field')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace('Miscellaneous', 'misc')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Traffic Operations', 'Traffic Engineering Design', 'Traffic Issue Investigation'], 'traffic')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Streets', 'Signals', 'Signs and Markings'], 'streets')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace('Trades', 'trades')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Stormwater', 'Storm Water', 'Storm Water Engineering'], 'storm')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Small Business', 'Food Establishments', 'Shops (Internal)', 'Shops'], 'business')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace('Workforce Development', 'workforce')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Customer Service', '311 Call Center', 'Director\'s Office Horizontal'], 'customer_service')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Land Development', 'Clean and Green', 'Urban Forestry', 'Natural Resources', 'Park Projects', 'Tree Crew', 'District 2', 'Clean and Green Natural Areas'], 'land')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace('Facility License', 'license')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Dangerous Premise','Historic Preservation', 'Engineering Division'], 'buildings')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Graffiti (IntExp)', 'General Sanitation', 'Graffiti'], 'cleanup')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Waste Collection', 'Solid Waste'], 'waste')
-    # replace with
-    df['REASONNAME'] = df['REASONNAME'].replace(['Waste Collection', 'Solid Waste'], 'waste')
-    df= df.rename(columns={
-                    'Category':'category', 'OPENEDDATETIME':'open_date', 'Dept': 'dept',
-                    'SLA_Date':'due_date', 'CLOSEDDATETIME': 'closed_date', 'Late (Yes/No)': 'is_late',
-                    'OBJECTDESC': 'address', 'REASONNAME': 'call_reason', 'TYPENAME': 'case_type', 
-                    'Council District': 'council_district', 'CASEID': 'case_id',
-                    'CaseStatus': 'case_status', 'SourceID':'source_id', 'XCOORD': 'longitude', 'YCOORD': 'latitude',
-                    'Report Starting Date': 'report_start_date', 'Report Ending Date': 'report_end_date'
-                      })
-    df['zipcode'] = df['address'].str.extract(r'(\d{5}\-?\d{0,4})')
-    
-    
-     # split data into train, validate, test
     train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify= None)
     train, validate = train_test_split(train_validate, 
                                        test_size=.3, 
                                        random_state=123, 
                                        stratify= None)
-    return df, train, validate, test
+    
+    # split train into X (dataframe, drop target) & y (series, keep target only)
+    X_train = train.drop(columns=['days_before_or_after_due'])
+    y_train = train['days_before_or_after_due']
+    
+    # split validate into X (dataframe, drop target) & y (series, keep target only)
+    X_validate = validate.drop(columns=['days_before_or_after_due'])
+    y_validate = validate['days_before_or_after_due']
+    
+    # split test into X (dataframe, drop target) & y (series, keep target only)
+    X_test = test.drop(columns=['days_before_or_after_due'])
+    y_test = test['days_before_or_after_due']
+    
+    # scale numeric variable
+    scaler = sklearn.preprocessing.StandardScaler()
+    scaler.fit(x_train)
 
-
+    x_train_scaled = scaler.transform(x_train)
+    x_validate_scaled = scaler.transform(x_validate)
+    x_test_scaled = scaler.transform(x_test)
+    
+    # Make y_values separate dataframes
+    y_train = pd.DataFrame(y_train)
+    y_validate = pd.DataFrame(y_validate)
+    y_test = pd.DataFrame(y_test)
+    
+    #Unscaled data for later
+    X_unscaled= pd.DataFrame(scaler.inverse_transform(X_test), columns=X_test.columns)
+    
+    return train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test, train_scaled, validate_scaled, test_scaled, X_uncaled
