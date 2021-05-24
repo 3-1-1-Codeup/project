e63ff8eb84017e54cefd54aeb56ebf5153801a91
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-05-24 13:40:51 -0500

    split function added

--- model.py
+++ model.py
@@ -66,6 +66,21 @@ def model_df():
     df= make_source_id_dummies(df)
 
     return df
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def split(df, stratify_by= 'level_of_delay'):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.2, random_state=319)
+        train, validate = train_test_split(train, test_size=.3, random_state=319)
+    else:
+        train, test = train_test_split(df, test_size=.2, random_state=319, stratify=df[stratify_by])
+        train, validate = train_test_split(train, test_size=.3, random_state=319, stratify=train[stratify_by])
+    return train, validate, test
+
 
 #------------------------------------
 def separate_y(train, validate, test):
