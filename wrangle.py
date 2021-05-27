d7f43b2f50d91ec0d0c863dfeff0bdca62cc28b8
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-05-27 09:52:29 -0500

    adding 3rd iteration stuff

--- wrangle.py
+++ wrangle.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import numpy as np
+import os
+
+from pydataset import data
+from datetime import date 
+from scipy import stats
+
+# turn off pink warning boxes
+import warnings
+warnings.filterwarnings("ignore")
+
+import sklearn
+
 from sklearn.model_selection import train_test_split
-import sklearn.preprocessing
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 
 #-----------------------------------------------------------------------------
